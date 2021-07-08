866b1b7e1f7dc363ba5d8dc6ad6cb151b87a5b71
Bruno Dias <brunodg.dev@gmail.com>
2021-07-08 14:38:34 -0300

    Messages being sent properly to Firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bevelchat/chat_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -7,21 +8,37 @@ void main() async {
   await Firebase.initializeApp();
   runApp(MyApp());
 
+  /**
+   * Escrevendo, Lendo, Alterando e Excluindo dados no Firebase:
+   * 
+   * .set() -> Escreve um dado novo no banco de dados - Create
+   * .get() -> Obtém as mensagens do banco (precisa de 'await') - Read
+   * .update() -> Atualiza campo de dado no banco - Update
+   * .delete() -> Exclui um campo ou dado no banco - Delete
+   * 
+   */
+
+  /*
   FirebaseFirestore.instance
-      .collection("col")
-      .doc("doc")
-      .set({"texto": "Bruno"});
+      .collection("mensagens")
+      .doc('kkDiByMOrz7897FSPajV')
+      .collection('arquivos')
+      .doc()
+      .set({'arquivo': 'foto.png'});
+  */
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Bevel Chat',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(),
+          primarySwatch: Colors.blue,
+          iconTheme: IconThemeData(
+            color: Colors.blue,
+          )),
+      home: ChatScreen(),
     );
   }
 }
---
+++ lib/text_composer.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class TextComposer extends StatefulWidget {
+  TextComposer(this.sendMessage);
+
+  Function(String) sendMessage;
+
+  @override
+  _TextComposerState createState() => _TextComposerState();
+}
+
+class _TextComposerState extends State<TextComposer> {
+  bool _isComposing = false;
+
+  final TextEditingController _controller = TextEditingController();
+
+  void _reset() {
+    _controller.clear();
+    setState(() {
+      _isComposing = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.photo_camera),
+          ),
+          Expanded(
+            child: TextField(
+              controller: _controller,
+              decoration:
+                  InputDecoration.collapsed(hintText: 'Enviar uma Mensagem'),
+              onChanged: (text) {
+                setState(() {
+                  _isComposing = text.isNotEmpty;
+                });
+              },
+              onSubmitted: (text) {
+                widget.sendMessage(text);
+                _reset();
+              },
+            ),
+          ),
+          IconButton(
+            onPressed: _isComposing
+                ? () {
+                    widget.sendMessage(_controller.text);
+                    _reset();
+                  }
+                : null,
+            icon: Icon(Icons.send),
+          ),
+        ],
+      ),
+    );
+  }
+}
