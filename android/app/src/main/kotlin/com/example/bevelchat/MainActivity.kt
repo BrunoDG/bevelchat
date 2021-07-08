6bc753648128ff78ca303c5303123312a5dc7aa6
Bruno Dias <brunodg.dev@gmail.com>
2021-07-08 12:01:30 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/bevelchat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bevelchat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+
+  FirebaseFirestore.instance
+      .collection("col")
+      .doc("doc")
+      .set({"texto": "Bruno"});
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
